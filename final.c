3793ee81d6bc1d2a5dcd05aafa1f60d702be493e
jjj47 <jjj47@zips.uakron.edu>
2021-11-29 21:59:17 -0500

    Read data from order.txt

--- final.c
+++ final.c
@@ -1,34 +1,98 @@
-/* File: final.c
- * 
- * 
- */
-
-#include <mpi.h>
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+#include <stdbool.h>
 
-int main(void) {
+int MAX_SIZE = 32;
+struct order{
+    char item[32];
+    double price;
+    char discount[32];
+};
 
-	int comm_sz, my_rank;
+typedef struct order Order;
+bool loop = true;
 
-	/* Start Up MPI */
-	MPI_Init(NULL, NULL);
+//int Read_Order(const char *filename, Order *orderArray);
+//void Print_Order(Order *orderArray, int orderSize);
 
-	/* Get the Number of Processes */
-	MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+int main(int argc, char* argv[]) {
+    Order *orderArray = (Order*)malloc(1000*sizeof(Order));
+    int userInput;
+    int orderSize;
+    int i = 0;
+    while(loop == true) {
+        printf("****************************************************\n");
+        printf("\tWelcome to John's and Allan's Shop\n");
+        printf("****************************************************\n");
+        printf("\t\t1) Search for User\n");
+        printf("\t\t2) Process an Order\n");
+        printf("\t\t3) View All Items\n");
+        printf("\t\t4) View Past Orders\n");
+        printf("\t\t5) Exit\n");
+        printf("****************************************************\n");
 
-	/* Get the Rank Among All the Processes */
-	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-
-	if (my_rank == 0) {
-		printf("=========================================\n");
-		printf("|| Welcome to the Final Project for PP ||\n");
-		printf("=========================================\n");
-	}
-
-	/* Shut Down MPI */
-	MPI_Finalize();
-
-	return 0;
+        printf("\t\tSelection - ");
+        scanf("%d", &userInput);
+        printf("****************************************************\n");
+    	switch(userInput) {
+            case 1 : 
+                printf("\tSearch for User\n");
+            	printf("****************************************************\n");
+                break;
+            case 2 : 
+                printf("\t\tProcess an Order\n");
+                FILE *fin;
+                fin = fopen("order.txt", "r");
+                if (fin == NULL) {
+                    printf("Cannot open file\n");
+                    break;
+                }
+    		while (!feof(fin)) {
+                printf("Scanning item %d\n", i);
+    		if (fscanf(fin, "%s $%lf Discount: %s",
+                    orderArray[i].item,
+            	    &orderArray[i].price,
+            	    orderArray[i].discount) == 3) {
+                    printf("Item %d: %s Price: $%0.2f Discount: %s \n",
+                            i,
+                            orderArray[i].item,
+                            orderArray[i].price,
+                            orderArray[i].discount);
+                   i++;
+                } else {
+                            printf("End of Order\n");
+                            break;
+                };
+                };
+    	        fclose(fin);
+    		orderSize = i;
+                printf("****************************************************\n");
+                break;
+            case 3 : 
+                printf("\t\tView All Items\n");
+    		printf("Printing Order\n");
+    		/*for (int i = 0; i < orderSize; i++) {
+        		Order *o = &orderArray[i];
+        		printf("Item %d: %s %d Discount: %s\n",
+            		    i,
+                            o->item,
+            		    o->price,
+                            o->discount);
+                }*/
+                printf("****************************************************\n");
+                break;
+            case 4 : 
+                printf("\t\tView Past Orders\n");
+                printf("****************************************************\n");
+                break;
+            case 5 : 
+                printf("\t\tClosing, Thank you!\n");
+                printf("****************************************************\n");
+                loop = false;
+                break;
+    	}
+    }
+    return 0;
 }
